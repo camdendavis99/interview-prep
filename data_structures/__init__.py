14169fced01908223d1f976bd8ed68269524ef7e
Camden Davis <camdendavis99@gmail.com>
2020-09-04 20:36:22 -0500

    Improve package formatting with __init__.py imports

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -1,1 +1,4 @@
 __all__ = ['LinkedList', 'LinkedListNode']
+
+from .LinkedList import LinkedList
+from .LinkedListNode import LinkedListNode
--- data_structures/iterators/__init__.py
+++ data_structures/iterators/__init__.py
@@ -1,1 +1,3 @@
 __all__ = ['LinkedListIterator']
+
+from .LinkedListIterator import LinkedListIterator
